661e3f7cde20a3b108681e721f8e7005c6797ce7
Jo <65770540+jowjomowjo@users.noreply.github.com>
2020-05-22 05:56:56 -0700

    Add files via upload

---
+++ src/views/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="login">
+    <b-jumbotron  header="Welcome to Vue Chat App"
+                  bg-variant="info"
+                  text-variant="white">
+    </b-jumbotron>
+    <b-container>
+      <b-row>
+        <b-col lg="4" md="3"></b-col>
+        <b-col lg="4" md="6">
+          <LoginForm />
+        </b-col>
+        <b-col lg="4" md="3"></b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import LoginForm from '@/components/LoginForm.vue'
+
+export default {
+  name: 'login',
+  components: {
+    LoginForm
+  }
+}
+</script>
